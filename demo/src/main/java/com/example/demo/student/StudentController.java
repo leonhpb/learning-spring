5a3fbce055db5a7cd227da6d938e3dff03b670ef
Leon Brum <leonhpb@gmail.com>
2022-12-05 11:26:09 -0300

    Autowires service class to controller

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,10 @@ import java.util.List;
 public class StudentController {
     private final StudentService studentService;
 
+    // Dependency injection: this @Autowired annotation will automatically instantiate the studentService needed
+    // StudentService class must be a Spring Bean, annotate its class with @Component or more specific to our case:
+    // @Service
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
